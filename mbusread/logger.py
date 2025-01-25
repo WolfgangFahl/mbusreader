7ccce822316f770868ba3fc5caf6dbb07a337c8a
Wolfgang Fahl <wf@bitplan.com>
2025-01-25 09:13:56 +0100

    makes Logger a singleton

--- mbusread/logger.py
+++ mbusread/logger.py
@@ -8,16 +8,19 @@ import logging
 
 
 class Logger:
-    @staticmethod
-    def setup_logger(debug: bool = False) -> logging.Logger:
-        """Configure logging"""
-        logger = logging.getLogger("MBusReader")
-        if debug:
-            logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        return logger
+    """Logger singleton for M-Bus reader"""
+    _logger = None
+
+    @classmethod
+    def setup_logger(cls, debug: bool = False) -> logging.Logger:
+        if cls._logger is None:
+            cls._logger = logging.getLogger("MBusReader")
+            if debug:
+                cls._logger.setLevel(logging.DEBUG)
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
+            handler.setFormatter(formatter)
+            cls._logger.addHandler(handler)
+        return cls._logger
